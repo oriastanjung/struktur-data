2627a1295705fd4c0b80565429623222be426041
O. Riastanjung <87843869+oriastanjung@users.noreply.github.com>
2021-09-26 18:08:17 +0700

    tugas struktur data array
    menambahkan method delete dan ubah menjadi array dynamic

--- Tugas 1 Array/main.c
+++ Tugas 1 Array/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void inputToData(int *ID_input, int *Age_input, char Name_input[][20], int *posisi_input);
 void showData(int *ID_input, int *Age_input, char Name_input[][20], int *posisi_input);
+void deleteData(int *ID_input, int *Age_input, char Name_input[][20], int *posisi_input);
 
 
 int main()
 {
-    int id[100];
-    int age[100];
-    char name[100][20];
     int option;
+
+    int *id;
+    id = (int *)malloc(100 * sizeof(int));
+
+    int *age;
+    age = (int *)malloc(100 * sizeof(int));
+    
+    char name[100][20];
+    
     int posisi = 0;
     menu :
     printf("\n\t\tActivity\n1. Input new data\n2. Delete data\n3. Show data list\n4. Exit Program\nYour choice: ");
@@ -24,6 +33,8 @@ int main()
     else if (option == 2)
     {
         printf("\nDelete Data\n");
+        
+        deleteData(id, age, name, &posisi);
         goto menu;
     }
 
@@ -63,6 +74,7 @@ void inputToData(int *ID_input, int *Age_input, char Name_input[][20], int *posi
     scanf("%d", Age_input + *posisi_input);
     *posisi_input = *posisi_input + 1;
 }
+
 void showData(int *ID_input, int *Age_input, char Name_input[][20], int *posisi_input)
 {
     for (int i = 0; i < *posisi_input; i++)
@@ -70,5 +82,39 @@ void showData(int *ID_input, int *Age_input, char Name_input[][20], int *posisi_
         printf("\nID : %d\t\t", *(ID_input + i));
         printf("Name : %s\t\t", *(Name_input + i));
         printf("Age : %d\t\t\n", *(Age_input + i));
+    }
+}
+
+void deleteData(int *ID_input, int *Age_input, char Name_input[][20], int *posisi_input)
+{
+    int isFound = 0;
+    int indexFound;
+    int search;
+
+   
+
+    printf("\nInput Customer ID to Delete : ");
+    scanf("%d", &search);
+
+    for (int i = 0; i <= *posisi_input; i++)
+    {
+        if (search == *(ID_input + i))
+        {
+            isFound = 1;
+            indexFound = i;
+        }
+    }
+    if (isFound == 1)
+    {
+        for (int c = indexFound; c < *posisi_input - 1; c++)
+        {
+            ID_input[c] = ID_input[c + 1];
+            Age_input[c] = Age_input[c + 1];
+            strcpy(Name_input[c], Name_input[c + 1]);
+        }
+        --(*posisi_input);
+        printf("\tData Deleted Successfully\n");
+    }else{
+         printf("\tID Not Found\n");
     }
 }
